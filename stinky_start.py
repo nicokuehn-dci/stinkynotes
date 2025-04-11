179835f6c3c6efed1b81c6f79b72978a110a6b77
Ekaterina Nestoklon <ekaterina.nestoklon@dci-student.org>
2025-04-11 14:07:30 +0200

    add user DONE

--- stinky_start.py
+++ stinky_start.py
@@ -4,16 +4,35 @@ import json
 import random
 import stringcolor
 
+def read_users(file_path):
+    with open(file_path, 'r') as file:
+        users = json.load(file)
+    return users
+
+def write_users(file_path, users):
+    with open(file_path, 'w') as file:
+        json.dump(users, file, indent=4)
+
+def create_user_json(user_id):
+    data = {
+            "user_id": user_id,
+            "notes": []
+        }
+    with open(f"./JSON/{user_id}.json", "w") as file:
+        json.dump(data, file, indent=4)
+    
 def show_menu():
     print("\n--- Menu ---")
-    print("1. Add/Edit User")
+    print("1. Add/Edit Userinfo")
     print("2. Delete User")
     print("2. Leave Note")
-    print("3. Publish Note")
+    print("3. Publish Note") #dont forget to add datetime stuff
     print("4. Edit Notes")
     print("5. Delete Notes")
     
     print("6. Exit")
+
+users = read_users('./JSON/stinky.json') #main dictionary with all users
 
 while True:
     show_menu()
@@ -21,6 +40,16 @@ while True:
 
     if choice == "1":
         print("You selected: Add/Edit User")
+        user_id = input("Add user ID: ")
+        fullname = input("Add Full Name: ")
+        password = input("Add Password: ")
+        users[user_id] = {
+            "fullname": fullname,
+            "password": password
+        }
+        write_users('./JSON/stinky.json', users)
+        create_user_json(user_id)
+        print(f"User {user_id} added successfully.")
         # Add/Edit User logic here
     elif choice == "2":
         print("You selected: Leave Note")
@@ -39,3 +68,5 @@ while True:
         break
     else:
         print("Invalid option. Please try again.")
+
+print(users)
