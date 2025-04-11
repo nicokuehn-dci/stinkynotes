419ae0c1ba654c574d2324e033cf1de384949143
Ekaterina Nestoklon <ekaterina.nestoklon@dci-student.org>
2025-04-11 14:47:31 +0200

    added Delete user

--- stinky_start.py
+++ stinky_start.py
@@ -16,21 +16,29 @@ def write_users(file_path, users):
 def create_user_json(user_id):
     data = {
             "user_id": user_id,
-            "notes": []
+            "notes": {}
         }
     with open(f"./JSON/{user_id}.json", "w") as file:
         json.dump(data, file, indent=4)
+
+def delete_user_json(user_id):
+    try:
+        os.remove(f"./JSON/{user_id}.json")
+        print(f"User {user_id}.json file deleted successfully.")
+    except FileNotFoundError:
+        print(f"User {user_id}.json file not found.")
+
     
 def show_menu():
     print("\n--- Menu ---")
-    print("1. Add/Edit Userinfo")
+    print("1. Add/Edit User")
     print("2. Delete User")
-    print("2. Leave Note")
-    print("3. Publish Note") #dont forget to add datetime stuff
-    print("4. Edit Notes")
-    print("5. Delete Notes")
+    print("3. Leave Note")
+    print("4. Publish Note") #dont forget to add datetime stuff
+    print("5. Edit Notes")
+    print("6. Delete Notes")
     
-    print("6. Exit")
+    print("0. Exit")
 
 users = read_users('./JSON/stinky.json') #main dictionary with all users
 
@@ -41,8 +49,13 @@ while True:
     if choice == "1":
         print("You selected: Add/Edit User")
         user_id = input("Add user ID: ")
-        fullname = input("Add Full Name: ")
-        password = input("Add Password: ")
+        if user_id in users:
+            print(f"User {user_id} already exists. You can change Fullname and Password.")
+            fullname = input("Change Full Name: ")
+            password = input("Change Password: ")
+        else:          
+            fullname = input("Add Full Name: ")
+            password = input("Add Password: ")
         users[user_id] = {
             "fullname": fullname,
             "password": password
@@ -50,10 +63,16 @@ while True:
         write_users('./JSON/stinky.json', users)
         create_user_json(user_id)
         print(f"User {user_id} added successfully.")
-        # Add/Edit User logic here
     elif choice == "2":
-        print("You selected: Leave Note")
-        # Leave Note logic here
+        print("You selected: Delete User")
+        user_id = input("Enter user ID to delete: ")
+        if user_id in users:
+            delete_user_json(user_id)
+            users.pop(user_id)
+            write_users('./JSON/stinky.json', users)
+            print(f"User {user_id} deleted successfully.")
+        else:
+            print(f"User {user_id} not found.")
     elif choice == "3":
         print("You selected: Publish Note")
         # Publish Note logic here
@@ -63,7 +82,7 @@ while True:
     elif choice == "5":
         print("You selected: Delete Notes")
         # Delete Notes logic here
-    elif choice == "6":
+    elif choice == "0":
         print("Exiting the program. Goodbye!")
         break
     else:
